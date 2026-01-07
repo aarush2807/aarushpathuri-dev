b32d88c2eb825853560b46dbfb1ecf18eadcd542
Aarush Pathuri <aarushvpathuri2807@gmail.com>
2026-01-06 20:55:41 -0600

    Wrap App component with BrowserRouter for routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // This imports your Tailwind styles
+import { BrowserRouter } from 'react-router-dom';
+import './index.css'; 
 import App from './App';
 
-// Create the root element where the React app will live
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Render the App component inside StrictMode for development best practices
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
